672a1050f0d381c592aaa50092e87be97ee4771b
Choi <dlsgur6965@naver.com>
2023-09-05 11:40:32 +0900

    modified:   app.py 	new file:   img_resize.py 	modified:   require.txt 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -22,20 +22,28 @@ def index():
             'comment_cnt' : data[7]
         }
         board_data_lst.append(data_dic)
-    
+    print(board_data_lst)
     if request.method == "POST":
         sorting_option = request.form['sorting_option']
-        
+    
         if sorting_option == "최신순":
             sorted_time_data_lst = sorted(board_data_lst, key=lambda x: x['create_time'])
-            return render_template("index.html", data_lst = sorted_time_data_lst)
-        
+            if 'id' in session:
+                return render_template("index.html", data_lst = sorted_time_data_lst, id = session['id'])
+            else:
+                return render_template("index.html", data_lst = sorted_time_data_lst)
         else:
             sorted_views_data_lst = sorted(board_data_lst, key=lambda x: x['views'], reverse=True)
-            return render_template("index.html", data_lst = sorted_views_data_lst)
-            
+            if 'id' in session:
+                return render_template("index.html", data_lst = sorted_views_data_lst, id = session['id'])
+            else:    
+                return render_template("index.html", data_lst = sorted_views_data_lst)
+        
     else:
-        return render_template("index.html",data_lst=board_data_lst)
+        if 'id' in session:
+            return render_template("index.html",data_lst=board_data_lst, id = session['id'])
+        else: 
+            return render_template("index.html",data_lst=board_data_lst)
 
 
 @app.route('/login', methods = ["GET", "POST"])
@@ -49,12 +57,16 @@ def login():
         isid = database.id_check(id_, pwd)
         if(isid) :
             session['id'] = id_
-            return redirect(url_for('index',id_info = id_))
+            return redirect(url_for('index'))
         else:
             flash("로그인 정보가 없습니다. 재로그인 해주세요. ")
             return redirect(url_for('login'))
 
-
+@app.route('/logout', methods = ["GET"])     
+def logout():
+    session.clear()
+    print("here")
+    return redirect(url_for('index'))
     #return id_info
 
 @app.route('/create')
